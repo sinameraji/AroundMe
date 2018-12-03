ec28949506c6d68c6a393634a1cb6dfb5e26ad12
Sina Meraji <sina.meraji@gmail.com>
2018-12-03 18:44:06 +0800

    first commit jeez i cant wait for the last commit

---
+++ app/src/main/java/my/edu/um/fsktm/arounddick/SingleArticle.java
@@ -0,0 +1,13 @@
+package my.edu.um.fsktm.arounddick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_article);
+    }
+}
