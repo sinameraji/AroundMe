c501b0204ad9740ca52eaeb73aa3485f07a50dc9
Sina Meraji <sina.meraji@gmail.com>
2018-12-04 02:53:55 +0800

    add actions  to app bar

--- app/src/main/java/my/edu/um/fsktm/arounddick/SingleArticle.java
+++ app/src/main/java/my/edu/um/fsktm/arounddick/SingleArticle.java
@@ -3,6 +3,8 @@ package my.edu.um.fsktm.arounddick;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,7 +73,33 @@ public class SingleArticle extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.top,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                //TODO: writing to bookmarks
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     private void populateCommentsList() {
         final ArrayList<Comment> arrayOfReviews= Comment.getReviews();
